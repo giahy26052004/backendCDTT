a3182244d9bd3cb2cd7dba369c34669b2d604513
=giahy26052004 <=giahy26052004@gmail.com>
2024-09-09 00:30:48 +0700

    se

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ app.use("/images", express.static(path.join(__dirname, "upload/images"))); // Sá
 app.post("/upload", upload.single("product"), (req, res) => {
   res.json({
     success: 1,
-    image_url: `http://localhost:${PORT}/images/${req.file.filename}`,
+    image_url: `https://backendcdtt.onrender.com/images/${req.file.filename}`,
   });
 });
 // addProduct
